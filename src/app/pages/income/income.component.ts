9561ccb8da82e7f72bf35a6ff8cd880ac03e4a4a
andrew.nash <akahadaka@gmail.com>
2017-10-01 00:10:18 +0200

    Write to db

--- src/app/pages/income/income.component.ts
+++ src/app/pages/income/income.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+// angular
+import { Component, OnInit }                  from '@angular/core';
+import { MdInputModule }                      from '@angular/material';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+// libraries
+import { AngularFireDatabase }                from "angularfire2/database";
+import { FirebaseObjectObservable }           from "angularfire2/database";
+
+// models
+import { Income }                             from '../../models/income';
 
 @Component({
-  selector: 'app-income',
-  templateUrl: './income.component.html',
-  styleUrls: ['./income.component.scss']
+    selector: 'app-income',
+    templateUrl: './income.component.html',
+    styleUrls: ['./income.component.scss']
 })
 export class IncomeComponent implements OnInit {
 
-  constructor() { }
+    incomeForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder, private db: AngularFireDatabase) {
+        //this.income = this.createIncome();
+        this.incomeForm = this.createForm();
+    }
+
+    ngOnInit() {
+    }
+
+    createForm(){
+        return this.fb.group({
+            invoice: ['', Validators.required],
+        });
+
+    }
+
+    /// Creates an Ad, then returns as an object
+    createIncome() {
+        let key = this.db.list('/income').push(this.incomeForm.value).key;
+        console.log(key);
+        //console.log(this.db.object('/income/' + key));
+        //return this.db.object('/income/' + key);
+    }
 
 }
