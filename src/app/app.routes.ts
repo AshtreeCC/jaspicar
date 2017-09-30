6e50c328d0226a57a23da770b988ad4f5d40f545
andrew.nash <akahadaka@gmail.com>
2017-09-30 17:47:49 +0200

    Add navigation and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,21 @@
 // angular
-import { Routes }               from '@angular/router';
+import { Routes }            from '@angular/router';
 
 // app
-import { HomeComponent }        from './pages/home/home.component';
+import { HomeComponent }     from './pages/home/home.component';
+import { IncomeComponent }   from './pages/income/income.component';
+import { ExpensesComponent } from './pages/expenses/expenses.component';
+import { StockComponent }    from './pages/stock/stock.component';
+import { VatComponent }      from './pages/vat/vat.component';
+import { TaxComponent }      from './pages/tax/tax.component';
+import { ContactComponent }  from './pages/contact/contact.component';
 
 export const APP_ROUTES: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'income', component: IncomeComponent },
+    { path: 'expenses', component: ExpensesComponent },
+    { path: 'stock', component: StockComponent },
+    { path: 'vat', component: VatComponent },
+    { path: 'tax', component: TaxComponent },
+    { path: 'contact', component: ContactComponent },
 ];
--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,8 +1,26 @@
 //components
-import { HomeComponent }      from './home/home.component';
+import { HomeComponent }     from './home/home.component';
+import { IncomeComponent }   from './income/income.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { StockComponent }    from './stock/stock.component';
+import { VatComponent }      from './vat/vat.component';
+import { TaxComponent }      from './tax/tax.component';
+import { ContactComponent }  from './contact/contact.component';
 
 export const APP_PAGE_COMPONENTS: any[] = [
-  HomeComponent
+    HomeComponent,
+    IncomeComponent,
+    ExpensesComponent,
+    StockComponent,
+    VatComponent,
+    TaxComponent,
+    ContactComponent
 ];
 
 export * from './home/home.component';
+export * from './income/income.component';
+export * from './expenses/expenses.component';
+export * from './stock/stock.component';
+export * from './vat/vat.component';
+export * from './tax/tax.component';
+export * from './contact/contact.component';
